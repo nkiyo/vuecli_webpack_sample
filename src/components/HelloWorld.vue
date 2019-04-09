5bfb6741a9b36d2d2afc85c8ae6a58dfb35d742e
nkiyo <nkiyo@hoge.com>
2019-04-10 07:51:39 +0900

    up

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -91,6 +91,9 @@ export default {
       msg: 'Welcome to Your Vue.js App',
     };
   },
+  methods: {
+    hello: x => x,
+  },
 };
 </script>
 
--- test/unit/specs/HelloWorld.spec.js
+++ test/unit/specs/HelloWorld.spec.js
@@ -8,4 +8,11 @@ describe('HelloWorld.vue', () => {
     expect(vm.$el.querySelector('.hello h1').textContent)
       .toEqual('Welcome to Your Vue.js App');
   });
+
+  it('should render correct contents', () => {
+    const Constructor = Vue.extend(HelloWorld);
+    const vm = new Constructor().$mount();
+    expect(vm.hello('hoge'))
+      .toEqual('hoge');
+  });
 });
